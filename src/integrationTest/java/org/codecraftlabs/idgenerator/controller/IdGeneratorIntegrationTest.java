75029d4f9cdb10edc772ec1669d8f6f7bf66c9de
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-08-21 08:36:32 +0200

    Add support for integration test tag

---
+++ src/integrationTest/java/org/codecraftlabs/idgenerator/controller/IdGeneratorIntegrationTest.java
@@ -0,0 +1,12 @@
+package org.codecraftlabs.idgenerator.controller;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("integration")
+public class IdGeneratorIntegrationTest {
+    @Test
+    public void fakeTest() {
+        // empty so far
+    }
+}
