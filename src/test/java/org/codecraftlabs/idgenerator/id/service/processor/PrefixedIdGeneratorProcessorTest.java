345d14dab192c555f1b4fb46417ebf6dd2e4753b
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-08-23 10:53:28 +0200

    Add one more unit test

---
+++ src/test/java/org/codecraftlabs/idgenerator/id/service/processor/PrefixedIdGeneratorProcessorTest.java
@@ -0,0 +1,55 @@
+package org.codecraftlabs.idgenerator.id.service.processor;
+
+import org.codecraftlabs.idgenerator.id.service.IdNotGeneratedException;
+import org.codecraftlabs.idgenerator.id.service.InvalidSeriesException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PrefixedIdGeneratorProcessorTest {
+    @Mock
+    private IdGenerator idGenerator;
+
+    private PrefixedIdGeneratorProcessor prefixedIdGeneratorProcessor;
+
+    @BeforeEach
+    public void setup() {
+        this.prefixedIdGeneratorProcessor = new PrefixedIdGeneratorProcessor(idGenerator);
+    }
+
+    @Test
+    public void when_sequence_not_found_should_raise_exception() {
+        // Setup mock
+        when(idGenerator.generateLongId("default"))
+                .thenThrow(IdNotGeneratedException.class);
+
+        assertThatExceptionOfType(IdNotGeneratedException.class)
+                .isThrownBy(() -> prefixedIdGeneratorProcessor.generateId("default"));
+    }
+
+    @Test
+    public void when_database_exception_happens_should_raise_exception() {
+        // Setup mock
+        when(idGenerator.generateLongId("default"))
+                .thenThrow(InvalidSeriesException.class);
+
+        assertThatExceptionOfType(InvalidSeriesException.class)
+                .isThrownBy(() -> prefixedIdGeneratorProcessor.generateId("default"));
+    }
+
+    @Test
+    public void when_ok_id_should_return() {
+        // Setup mock
+        when(idGenerator.generateLongId(anyString())).thenReturn(100L);
+        var result = prefixedIdGeneratorProcessor.generateId("default");
+        assertThat(result).isEqualTo("STD000000000000100");
+    }
+}
