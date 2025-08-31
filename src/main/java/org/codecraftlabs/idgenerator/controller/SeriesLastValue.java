de2bbc215703b5cb4abc97e1c1198078b32bb53c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-08-31 22:57:14 +0200

    Initial commit

---
+++ src/main/java/org/codecraftlabs/idgenerator/controller/SeriesLastValue.java
@@ -0,0 +1,13 @@
+package org.codecraftlabs.idgenerator.controller;
+
+public class SeriesLastValue {
+    private long newLastValue;
+
+    public void setNewLastValue(long newLastValue) {
+        this.newLastValue = newLastValue;
+    }
+
+    public long getNewLastValue() {
+        return newLastValue;
+    }
+}
